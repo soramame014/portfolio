7e426081fd7abf6cb66758ddb35954ed924cb7d7
EC2 Default User <ec2-user@ip-172-31-3-49.ap-northeast-1.compute.internal>
2022-10-06 16:49:15 +0000

    <削除

--- db/migrate/20221005060525_remove_last_name_fromcustomers.rb
+++
@@ -1,4 +0,0 @@
-class RemoveLastNameFromcustomers < ActiveRecord::Migration[6.1]
-  def change
-  end
-end
--- db/migrate/20221005060525_tmp.rb
+++ db/migrate/20221005060525_tmp.rb
@@ -1,6 +1,4 @@
 class RemoveLastNameFromCustomers < ActiveRecord::Migration[6.1]
   def change
-     remove_column :customers, :last_name, :string
   end
 end
-›
